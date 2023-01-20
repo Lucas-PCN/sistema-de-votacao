38942d7965cd3fea508e8098702648ba0fec71dc
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-01-20 15:41:38 -0300

    Feat: adiciona classe Pessoa, PessoaEleitora e PessoaCandidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,26 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
-
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+  
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+  
+  public String getNome() {
+    return super.nome;
+  }
+  
+  public void setNome(String nome) {
+    super.nome = nome;
+  }
+  
+  public String getCpf() {
+    return cpf;
+  }
+  
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
